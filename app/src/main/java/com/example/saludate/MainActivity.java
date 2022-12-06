a4151f6cf53496c988ed06c851c8a194689a0a51
Waxiest15 <waxiest15@gmail.com>
2022-12-06 08:37:30 -0600

    Actualizacion 06/12/20222
    Cambio de color de fondo a las pantallas
    correccion de color a textos
    corecciones menores

--- app/src/main/java/com/example/saludate/MainActivity.java
+++ app/src/main/java/com/example/saludate/MainActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
+import com.example.saludate.databinding.ActivityMainBinding;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
@@ -16,8 +18,6 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.saludate.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -53,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(individual);
             }
         });
+
+        
 
         FloatingActionButton fab = findViewById(R.id.fab_patientAdd_fragment);
         fab.setOnClickListener(new View.OnClickListener() {
