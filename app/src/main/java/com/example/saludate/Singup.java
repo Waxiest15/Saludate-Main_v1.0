847174a00c796e5d6e839496b7b95da1859b6042
MarcoAntonioOrnelas <MarcoAntonioOrnelas@users.noreply.github.com>
2022-12-11 13:09:55 -0600

    Login, SignUp y Authentification

--- app/src/main/java/com/example/saludate/Singup.java
+++ app/src/main/java/com/example/saludate/Singup.java
@@ -1,24 +1,75 @@
 package com.example.saludate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Singup extends AppCompatActivity {
     Intent intent;
+    EditText nom, email, pass;
+    Button btn;
+    DatabaseReference dbRef;
+    FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        firebaseAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.EmailSignUpM);
+        pass = findViewById(R.id.PasswordSignupM);
+        nom = findViewById(R.id.NameSignUpM);
+        dbRef = FirebaseDatabase.getInstance().getReference();
 
-        Button btn = findViewById(R.id.button_signup);
+        btn = findViewById(R.id.button_signup);
         btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent individual = new Intent(v.getContext(), MainActivity.class);
-                startActivity(individual);
+                String mail = email.getText().toString();
+                String password = pass.getText().toString();
+                String name = nom.getText().toString();
+                firebaseAuth.createUserWithEmailAndPassword(mail,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            String id = firebaseAuth.getCurrentUser().getUid();
+                            Map<String, Object> map= new HashMap<>();
+                            map.put("name", name );
+                            map.put("email",mail);
+                            map.put("password",password);
+                            map.put("Cel", "");
+                            map.put("id", id);
+                            map.put("Turno","");
+                            dbRef.child("Enfermeras").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task2) {
+                                 if (task2.isSuccessful()){
+                                     Toast.makeText(Singup.this, "Usuario creado con Exito",Toast.LENGTH_SHORT);
+                                     finish();
+                                 }
+                                }
+                            });
+                        }else{
+                            Toast.makeText(Singup.this, "Usuario no creado",Toast.LENGTH_SHORT);
+                        }
+                    }
+                });
             }
         });
     }
@@ -26,6 +77,7 @@ public class Singup extends AppCompatActivity {
         intent=new Intent(getApplicationContext(),Login.class);
         startActivity(intent);
     }
+
 
 
 
