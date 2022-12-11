ee4945595ee6b91aee10fa640e48e3d8dd79058d
Waxiest15 <waxiest15@gmail.com>
2022-12-10 19:55:09 -0600

    Ya jala el recycleview
    dios mio

---
+++ app/src/main/java/com/example/saludate/Paciente_Recycle.java
@@ -0,0 +1,31 @@
+package com.example.saludate;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class Paciente_Recycle {
+
+
+
+    private String cel;
+
+    private String name, last, last2, age, bed;
+
+    
+
+    public Paciente_Recycle() {
+
+    }
+
+    public Paciente_Recycle(String cel) {
+        this.cel = cel;
+    }
+
+    public void setCel(String cel) {
+        this.cel = cel;
+    }
+
+    public String getCel() {
+        return cel;
+    }
+}
--- app/src/main/java/com/example/saludate/Pacientes.java
+++ app/src/main/java/com/example/saludate/Pacientes.java
@@ -16,6 +16,40 @@ public class Pacientes extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pacientes);
 
+<<<<<<< Updated upstream
+=======
+        recyclerView = findViewById((R.id.recycleV_patients));
+        databaseReference = FirebaseDatabase.getInstance().getReference("Enfermeras");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list = new ArrayList<>();
+        Log.d("LIST", list.toString());
+        Log.d("REFERENCE", databaseReference.toString());
+
+        patientAdapter = new PatientAdapter(this, list);
+        recyclerView.setAdapter(patientAdapter);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    //Log.d("SNAP", snapshot.getValue((Paci)));
+                    Paciente_Recycle paciente = dataSnapshot.getValue(Paciente_Recycle.class);
+                    list.add(paciente);
+                }
+                patientAdapter.setPatientList(list);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+>>>>>>> Stashed changes
         FloatingActionButton button = findViewById(R.id.fab_patientAdd_fragment);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
